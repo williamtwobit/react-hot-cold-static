613682c24b3566a906a19655264c832b01224845
Avi <avirzajac@gmail.com>
2017-06-22 16:21:53 +0000

    Initial work on state

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,59 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component{
+    Constructor(props){
+        super(props);
+        this.state={
+            guesses:[],
+            correctAnswer:'3',
+            feedback:'Make your guess!',
+            poodle:false //This is the modal for the what?
+        };
+        newGame(){
+            this.setState({
+                guesses:[],
+                correctAnswer:'3',
+                feedback:'Make your guess!'
+                poodle:false
+            })
+        }
+        submitGuess(userGuess){
+            this.setState({
+                guesses:[...this.state.guesses,userGuess]
+                feedback: 
+            })
+            let submittedGuess=userGuess-correctAnswer;
+            if(userGuess===correctAnswer){
+                feedback="You Won. Click new game to play again";
+                return winner();
+            }
+            else if(submittedGuess<10){
+                feedback="Hot";
+            }
+            else if(submittedGuess<20){
+                feedback="Kinda hot";
+            }
+            else if(submittedGuess<30){
+                feedback="Less than warm";
+            }
+            else if(submittedGuess<40){
+                feedback="Cold";
+            }
+        }
+        poodle(clickMe){
+            this.setState(){
+                poodle: true;
+            }
+        }
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback={this.state.feedback} />
+                <GuessCount count={3} />
+                <GuessList guesses={this.state.guesses} />
+            </div>
+        );
+    }
 }
 
